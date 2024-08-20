07c0580ecd248f7406aab6aad621db6514a25463
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-20 17:27:12 -0300

    feat: add initial pages structure

---
+++ src/frontend/src/app/analysis/page.js
@@ -0,0 +1,7 @@
+export default function AnalysisPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>oi tudo bem</p>
+    </main>
+  );
+}
