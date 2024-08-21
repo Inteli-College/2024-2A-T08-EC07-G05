5e14bf6b8211bd346687ad9ff88cb71541c833c2
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-21 17:31:52 -0300

    feat: base button component

--- src/frontend/src/app/prediction/page.js
+++
@@ -1,7 +0,0 @@
-export default function AnalysisPage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>oi tudo bem</p>
-    </main>
-  );
-}
---
+++ src/frontend/src/app/prediction/page.jsx
@@ -0,0 +1,12 @@
+import BaseButton from "@/components/baseButton";
+
+export default function AnalysisPage() {
+  return (
+    <main className="flex min-h-screen flex-col justify-between p-10">
+      <div className="w-full flex justify-end">
+        <BaseButton text='Ok'/>
+      </div>
+      <p>oi tudo bem</p>
+    </main>
+  );
+}
