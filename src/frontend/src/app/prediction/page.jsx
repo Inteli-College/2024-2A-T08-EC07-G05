9d475d1853465669aef2bfc777843c53e1e1e8c4
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-22 16:37:30 -0300

    feat: add input bar

--- src/frontend/src/app/prediction/page.jsx
+++ src/frontend/src/app/prediction/page.jsx
@@ -1,9 +1,11 @@
 import BaseButton from "@/components/baseButton";
+import InputBar from "@/components/inputBar";
 
-export default function AnalysisPage() {
+export default function PredictionPage() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-10">
-      <div className="w-full flex justify-end">
+      <div className="h-12 w-full flex justify-evenly">
+        <InputBar />
         <BaseButton text='Ok'/>
       </div>
       <p>oi tudo bem</p>
