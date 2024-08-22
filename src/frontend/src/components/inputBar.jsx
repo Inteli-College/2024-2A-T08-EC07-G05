8ab78e0db49d971bc4e97a406dfa165dcaf9967a
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-22 16:37:30 -0300

    feat: add input bar

---
+++ src/frontend/src/components/inputBar.jsx
@@ -0,0 +1,8 @@
+export default function InputBar() {
+    return (
+        <>
+            <input type="text" id="knr_input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-blue-500 focus:border-blue-500 w-3/4 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Insira o KNR" required />
+        </>
+    );
+  }
+  
