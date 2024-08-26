f046a49c3e25766fd0bd5410d4091c46d590293b
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-26 17:17:13 -0300

    feat: add home button

--- src/frontend/src/components/baseButton.jsx
+++ src/frontend/src/components/baseButton.jsx
@@ -1,8 +1,13 @@
-export default function BaseButton({ text }) {
-    return (
-      <button type="button" className="text-white w-1/6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-3xl text-2xl px-5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-        {text}
-      </button>
-    );
-  }
-  
+import Image from 'next/image';
+
+export default function BaseButton({ text, icon }) {
+  return (
+    <button
+      type="button"
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-3xl text-2xl px-5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 flex items-center gap-2"
+    >
+      {icon && <Image src={icon} alt="icon" width={20} height={20} />}
+      {text}
+    </button>
+  );
+}
