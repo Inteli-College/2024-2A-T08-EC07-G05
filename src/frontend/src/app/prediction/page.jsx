13f4e7c10bbe1f55464cc0948ec5515e531e798f
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-26 17:17:13 -0300

    feat: add home button

--- src/frontend/src/app/prediction/page.jsx
+++ src/frontend/src/app/prediction/page.jsx
@@ -1,10 +1,12 @@
 import BaseButton from "@/components/baseButton";
 import InputBar from "@/components/inputBar";
+import IconHome from "@/../public/icone_home.svg"
 
 export default function PredictionPage() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-10">
       <div className="h-12 w-full flex justify-evenly">
+        <BaseButton icon={IconHome} />
         <InputBar />
         <BaseButton text='Ok'/>
       </div>
