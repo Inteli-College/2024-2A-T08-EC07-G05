f77541a8ed1e1a75055413d2049ca4354f4adf84
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-28 12:19:18 -0300

    feat: add initial prediction page structure

--- src/frontend/src/app/prediction/page.jsx
+++ src/frontend/src/app/prediction/page.jsx
@@ -1,14 +1,23 @@
+'use client';
+
 import BaseButton from "@/components/baseButton";
 import InputBar from "@/components/inputBar";
 import IconHome from "@/../public/icone_home.svg"
 
 export default function PredictionPage() {
+
+  const handleButtonClick = () => {
+
+    console.log("Teste")
+
+  }
+
   return (
     <main className="flex min-h-screen flex-col justify-between p-10">
       <div className="h-12 w-full flex justify-evenly">
         <BaseButton icon={IconHome} />
         <InputBar />
-        <BaseButton text='Ok'/>
+        <BaseButton text='Ok' onClick={handleButtonClick} />
       </div>
       <p>oi tudo bem</p>
     </main>
