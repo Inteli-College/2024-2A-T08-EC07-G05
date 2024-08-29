b418ee8a56effac3a4bfec17a1dd07846951b38b
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-29 17:58:25 -0300

    feat: prediction output card

--- src/frontend/src/app/prediction/page.jsx
+++ src/frontend/src/app/prediction/page.jsx
@@ -6,25 +6,23 @@ import InputBar from "@/components/inputBar";
 import IconHome from "@/../public/icone_home.svg";
 import { columns } from "@/components/columns";
 import { DataTable } from "@/components/ui/data-table";
+import BaseCard from '@/components/baseCard';
 
 export default function PredictionPage() {
   const [data, setData] = useState(null);  // Estado para armazenar os dados
   const [showTable, setShowTable] = useState(false);  // Estado para controlar a visibilidade da tabela
   const [inputValue, setInputValue] = useState('');  // Estado para armazenar o valor do input
+  const [predictionTextOutput, setPredictionTextOutput] = useState('');  // Novo estado para armazenar o texto da predição
 
   const handleButtonClick = async () => {
-
     console.log("Valor de KNR inserido: " + inputValue); 
     
     const response = await fetch(
       "http://127.0.0.1:8000/predict/", {
-        
         method: "POST",
-
         body: JSON.stringify({
             knr: inputValue
           }),
-
         headers: {
           'Content-type': 'application/json; charset=UTF-8'
         }
@@ -35,23 +33,30 @@ export default function PredictionPage() {
       return;
     }
 
-    const tableInfo = await response.json();
-    console.log(tableInfo);
+    const responseJson = await response.json();
+    console.log(responseJson);
 
-    tableInfo['KNR'] = inputValue
-    tableInfo['model'] = 'It-Cross'
-
-    console.log("Resultado da predição: " + tableInfo['prediction'])
-
-    setData([tableInfo]);  // Armazena apenas o primeiro resultado no estado (note que é um array)
+    responseJson['KNR'] = inputValue;  // salvando o KNR como valor do json p/ a tabela poder puxar esse valor
+    responseJson['model'] = 'It-Cross';  // para o nosso projeto, o modelo sempre vai ser It-Cross
+    
+    console.log("Resultado da predição: " + responseJson['prediction']);
+    
+    setData([responseJson]);  // Armazena apenas o primeiro resultado no estado (note que é um array)
     setShowTable(true);  // Mostra a tabela
+    
+    // Atualiza o estado com base na predição
+    if (responseJson['prediction'] == 1) {
+      setPredictionTextOutput("Suscetível a teste");
+    } else {
+      setPredictionTextOutput("NÃO suscetível a teste");
+    }
   };
-
+  
   // Função de callback pra pegar o input
   const handleInputChange = (event) => {
     setInputValue(event.target.value);  // Atualiza o valor do input
   };
-
+  
   return (
     <main className="flex min-h-screen flex-col justify-between p-10">
 
@@ -63,7 +68,12 @@ export default function PredictionPage() {
 
       {showTable && data && <DataTable columns={columns} data={data} />}  {/* Renderiza a tabela se showTable for true */}
 
-      <p>oiiiiii tudo bem</p>
+      {showTable && data && (
+        <div className="flex justify-center items-center mt-6">  {/* Centraliza o BaseCard */}
+          <BaseCard text={predictionTextOutput} />
+        </div>
+      )}
+
     </main>
   );
 }
