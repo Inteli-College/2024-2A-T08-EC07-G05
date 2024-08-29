d7a6cb336a2928b4de449e4afa90923c56b50dd9
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-28 22:49:39 -0300

    feat: prediction page initial integration to backend

--- src/frontend/src/app/prediction/page.jsx
+++ src/frontend/src/app/prediction/page.jsx
@@ -10,21 +10,41 @@ import { DataTable } from "@/components/ui/data-table";
 export default function PredictionPage() {
   const [data, setData] = useState(null);  // Estado para armazenar os dados
   const [showTable, setShowTable] = useState(false);  // Estado para controlar a visibilidade da tabela
+  const [inputValue, setInputValue] = useState('');  // Estado para armazenar o valor do input
 
   const handleButtonClick = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const jsonData = await response.json();
+    console.log(inputValue);  // Aqui você pode usar o valor do input
+    const response = await fetch("http://127.0.0.1:8000/predict/", {
+      method: "POST",
+      body: JSON.stringify({
+        knr: inputValue  // Certifique-se de que o campo `knr` é o que a API espera
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'  // Define o cabeçalho para JSON
+      }
+    });
 
-    console.log(jsonData[0]);
-    setData([jsonData[0]]);  // Armazena apenas o primeiro resultado no estado (note que é um array)
-    setShowTable(true);  // Mostra a tabela
+    if (!response.ok) {
+      console.error("Erro na requisição:", response.statusText);
+      return;
+    }
+
+    const jsonData = await response;
+    console.log(jsonData);
+    //setData([jsonData[0]]);  // Armazena apenas o primeiro resultado no estado (note que é um array)
+    //setShowTable(true);  // Mostra a tabela
+  };
+
+  // Função de callback para capturar o valor do input
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);  // Atualiza o valor do input
   };
 
   return (
     <main className="flex min-h-screen flex-col justify-between p-10">
       <div className="h-12 w-full flex justify-evenly mb-6">
         <BaseButton icon={IconHome} />
-        <InputBar />
+        <InputBar onChange={handleInputChange} />  {/* Passa a função de callback */}
         <BaseButton text='Ok' onClick={handleButtonClick} />
       </div>
       {showTable && data && <DataTable columns={columns} data={data} />}  {/* Renderiza a tabela se showTable for true */}
