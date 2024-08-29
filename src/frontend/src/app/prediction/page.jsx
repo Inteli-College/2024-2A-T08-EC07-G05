de8ecfed9a3b0d79c2cb87e8748f8780f02310a0
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-29 17:12:35 -0300

    feat: table returning color by specified KNR

--- src/frontend/src/app/prediction/page.jsx
+++ src/frontend/src/app/prediction/page.jsx
@@ -13,42 +13,57 @@ export default function PredictionPage() {
   const [inputValue, setInputValue] = useState('');  // Estado para armazenar o valor do input
 
   const handleButtonClick = async () => {
-    console.log(inputValue);  // Aqui você pode usar o valor do input
-    const response = await fetch("http://127.0.0.1:8000/predict/", {
-      method: "POST",
-      body: JSON.stringify({
-        knr: inputValue  // Certifique-se de que o campo `knr` é o que a API espera
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8'  // Define o cabeçalho para JSON
-      }
-    });
+
+    console.log("Valor de KNR inserido: " + inputValue); 
+    
+    const response = await fetch(
+      "http://127.0.0.1:8000/predict/", {
+        
+        method: "POST",
+
+        body: JSON.stringify({
+            knr: inputValue
+          }),
+
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      });
 
     if (!response.ok) {
       console.error("Erro na requisição:", response.statusText);
       return;
     }
 
-    const jsonData = await response;
-    console.log(jsonData);
-    //setData([jsonData[0]]);  // Armazena apenas o primeiro resultado no estado (note que é um array)
-    //setShowTable(true);  // Mostra a tabela
+    const tableInfo = await response.json();
+    console.log(tableInfo);
+
+    tableInfo['KNR'] = inputValue
+    tableInfo['model'] = 'It-Cross'
+
+    console.log("Resultado da predição: " + tableInfo['prediction'])
+
+    setData([tableInfo]);  // Armazena apenas o primeiro resultado no estado (note que é um array)
+    setShowTable(true);  // Mostra a tabela
   };
 
-  // Função de callback para capturar o valor do input
+  // Função de callback pra pegar o input
   const handleInputChange = (event) => {
     setInputValue(event.target.value);  // Atualiza o valor do input
   };
 
   return (
     <main className="flex min-h-screen flex-col justify-between p-10">
+
       <div className="h-12 w-full flex justify-evenly mb-6">
         <BaseButton icon={IconHome} />
         <InputBar onChange={handleInputChange} />  {/* Passa a função de callback */}
         <BaseButton text='Ok' onClick={handleButtonClick} />
       </div>
+
       {showTable && data && <DataTable columns={columns} data={data} />}  {/* Renderiza a tabela se showTable for true */}
-      <p>oi tudo bem</p>
+
+      <p>oiiiiii tudo bem</p>
     </main>
   );
 }
