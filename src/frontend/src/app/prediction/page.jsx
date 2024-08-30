8554ab1a8b4c57011b0146f6c247bf9f2767b555
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-08-30 10:09:38 -0300

    feat: different colors for different prediction results + new home page icon

--- src/frontend/src/app/prediction/page.jsx
+++ src/frontend/src/app/prediction/page.jsx
@@ -9,20 +9,21 @@ import { DataTable } from "@/components/ui/data-table";
 import BaseCard from '@/components/baseCard';
 
 export default function PredictionPage() {
-  const [data, setData] = useState(null);  // Estado para armazenar os dados
-  const [showTable, setShowTable] = useState(false);  // Estado para controlar a visibilidade da tabela
-  const [inputValue, setInputValue] = useState('');  // Estado para armazenar o valor do input
-  const [predictionTextOutput, setPredictionTextOutput] = useState('');  // Novo estado para armazenar o texto da predição
+  const [data, setData] = useState(null);
+  const [showTable, setShowTable] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [predictionTextOutput, setPredictionTextOutput] = useState('');
+  const [cardColor, setCardColor] = useState('bg-blue-500');  // Estado para a cor do BaseCard
 
   const handleButtonClick = async () => {
-    console.log("Valor de KNR inserido: " + inputValue); 
-    
+    console.log("Valor de KNR inserido: " + inputValue);
+
     const response = await fetch(
       "http://127.0.0.1:8000/predict/", {
         method: "POST",
         body: JSON.stringify({
-            knr: inputValue
-          }),
+          knr: inputValue
+        }),
         headers: {
           'Content-type': 'application/json; charset=UTF-8'
         }
@@ -36,44 +37,44 @@ export default function PredictionPage() {
     const responseJson = await response.json();
     console.log(responseJson);
 
-    responseJson['KNR'] = inputValue;  // salvando o KNR como valor do json p/ a tabela poder puxar esse valor
-    responseJson['model'] = 'It-Cross';  // para o nosso projeto, o modelo sempre vai ser It-Cross
-    
+    responseJson['KNR'] = inputValue;
+    responseJson['model'] = 'It-Cross';
+
     console.log("Resultado da predição: " + responseJson['prediction']);
-    
-    setData([responseJson]);  // Armazena apenas o primeiro resultado no estado (note que é um array)
-    setShowTable(true);  // Mostra a tabela
-    
+
+    setData([responseJson]);
+    setShowTable(true);
+
     // Atualiza o estado com base na predição
     if (responseJson['prediction'] == 1) {
       setPredictionTextOutput("Suscetível a teste");
+      setCardColor('bg-red-600');  // Define a cor verde para resultado positivo
     } else {
       setPredictionTextOutput("NÃO suscetível a teste");
+      setCardColor('bg-green-600');  // Define a cor vermelha para resultado negativo
     }
   };
-  
-  // Função de callback pra pegar o input
+
   const handleInputChange = (event) => {
-    setInputValue(event.target.value);  // Atualiza o valor do input
+    setInputValue(event.target.value);
   };
-  
+
   return (
     <main className="flex min-h-screen flex-col justify-between p-10">
 
       <div className="h-12 w-full flex justify-evenly mb-6">
         <BaseButton icon={IconHome} />
-        <InputBar onChange={handleInputChange} />  {/* Passa a função de callback */}
+        <InputBar onChange={handleInputChange} />
         <BaseButton text='Ok' onClick={handleButtonClick} />
       </div>
 
-      {showTable && data && <DataTable columns={columns} data={data} />}  {/* Renderiza a tabela se showTable for true */}
+      {showTable && data && <DataTable columns={columns} data={data} />}
 
       {showTable && data && (
-        <div className="flex justify-center items-center mt-6">  {/* Centraliza o BaseCard */}
-          <BaseCard text={predictionTextOutput} />
+        <div className="flex justify-center items-center mt-6">
+          <BaseCard text={predictionTextOutput} color={cardColor} />  {/* Passa a cor como prop */}
         </div>
       )}
-
     </main>
   );
 }
