424e876c013302a6068aa1fbeb5dd3966121be5b
LuizaRubim <luiza.rubim@sou.inteli.edu.br>
2024-09-04 23:01:28 -0300

    knr input validation

--- src/frontend/src/app/page.js
+++ src/frontend/src/app/page.js
@@ -1,14 +1,23 @@
-import BaseButton from "@/components/baseButton";
 import Link from "next/link";
+import { Button } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <img src="/logo.svg" alt="Logo" className="w-150" />
-      <img src="/T-cross-prata.png" alt="T-cross" className="w-64" />
+    <main className="flex min-h-screen flex-row items-center p-10 h-3/6 space-x-6 ">
+      <div className=" flex flex-col space-y-4 items-center">
+      <img src="/logo.svg" alt="Logo" className="w-120" />
+      <p>(adicionar pequena descrição da aplicação)</p> 
       <div className="flex space-x-4">
-        <Link href="/prediction" className=" text-white p-3 text-lg mx-auto bg-gray-600 shadow-lg rounded-md">Predição</Link>
-        <Link href="/history" className=" text-white p-3 text-lg mx-auto bg-gray-600 shadow-lg rounded-md">Histórico</Link>
+        <Button variant='outline' colorScheme='blue' >
+          <Link href="/history" >Histórico de registros</Link>
+        </Button>
+        <Button colorScheme='blue'>
+          <Link href="/prediction" >Predição de falhas</Link>
+        </Button>
+              </div>
+      </div>
+      <div>
+      <img src="/T-cross-prata.png" alt="T-cross" className="w-100" />
       </div>
     </main>
   );
