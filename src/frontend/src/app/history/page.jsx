c60d4d6d7dbdac07629d01af0a6a59f1c92569f9
LuizaRubim <luiza.rubim@sou.inteli.edu.br>
2024-09-06 10:40:44 -0300

    total time sum and preditcion and result integration

--- src/frontend/src/app/history/page.jsx
+++ src/frontend/src/app/history/page.jsx
@@ -9,43 +9,48 @@ import { useState } from 'react';
 function HistoryPage() {
   const [data, setData] = useState(null)
   const [isLoading, setLoading] = useState(true)
-  const renderTableRow = (data) => (
-    console.log("data:", data),
-    <tr className="table-row">
-      <td className="border px-4 py-2">
-        <table className="w-full bg-white">
-          <thead>
-            <tr className="table-header">
-              <th className="border px-2 py-1 text-left">ZP5</th>
-              <th className="border px-2 py-1 text-left">ZP5A</th>
-              <th className="border px-2 py-1 text-left">ZP61</th>
-              <th className="border px-2 py-1 text-left">ZP6 / ZP62</th>
-              <th className="border px-2 py-1 text-left">CAB</th>
-              <th className="border px-2 py-1 text-left">ZP7</th>
-              <th className="border px-2 py-1 text-left">ROD</th>
-              <th className="border px-2 py-1 text-left">AGUA</th>
-              <th className="border px-2 py-1 text-left">ZP8</th>
-              <th className="border px-2 py-1 text-left">Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="border px-2 py-1 text-left"></td>
-              <td className="border px-2 py-1 text-left">mock</td>
-              <td className="border px-2 py-1 text-left">mock</td>
-              <td className="border px-2 py-1 text-left">mock</td>
-              <td className="border px-2 py-1 text-left">mock</td>
-              <td className="border px-2 py-1 text-left">mock</td>
-              <td className="border px-2 py-1 text-left">mock</td>
-              <td className="border px-2 py-1 text-left">mock</td>
-              <td className="border px-2 py-1 text-left">mock</td>
-              <td className="border px-2 py-1 text-left">mock</td>
-            </tr>
-          </tbody>
-        </table>
-      </td>
-    </tr>
-  );
+  const renderTableRow = (data) => {
+    console.log("data:", data);
+    const total = Object.values(data).reduce((acc, value) => acc + value, 0);
+    return (
+      <tr className="table-row">
+        <td className="border px-4 py-2">
+          <table className="w-full bg-white">
+            <thead>
+              <tr className="table-header">
+                <th className="border px-2 py-1 text-left">ZP5</th>
+                <th className="border px-2 py-1 text-left">ZP5A</th>
+                <th className="border px-2 py-1 text-left">ZP61</th>
+                <th className="border px-2 py-1 text-left">ZP6 / ZP62</th>
+                <th className="border px-2 py-1 text-left">CAB</th>
+                <th className="border px-2 py-1 text-left">ZP7</th>
+                <th className="border px-2 py-1 text-left">ROD</th>
+                <th className="border px-2 py-1 text-left">AGUA</th>
+                <th className="border px-2 py-1 text-left">ZP8</th>
+                <th className="border px-2 py-1 text-left">ESPC</th>
+                <th className="border px-2 py-1 text-left">Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border px-2 py-1 text-left">{data.ZP5}</td>
+                <td className="border px-2 py-1 text-left">{data.ZP5A}</td>
+                <td className="border px-2 py-1 text-left">{data.ZP61}</td>
+                <td className="border px-2 py-1 text-left">{data.ZP6|data.ZP62}</td>
+                <td className="border px-2 py-1 text-left">{data.CAB}</td>
+                <td className="border px-2 py-1 text-left">{data.ZP7}</td>
+                <td className="border px-2 py-1 text-left">{data.ROD}</td>
+                <td className="border px-2 py-1 text-left">{data.AGUA}</td>
+                <td className="border px-2 py-1 text-left">{data.ZP8}</td>
+                <td className="border px-2 py-1 text-left">{data.ESPC}</td>
+                <td className="border px-2 py-1 text-left">{total.toFixed(2)}</td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    );
+  };
 
   const homeClick = () => {
     window.location.href = '/';
@@ -80,8 +85,8 @@ function HistoryPage() {
             <tr className="table-header">
               <th className="border px-4 py-2 text-left">KNR</th>
               <th className="border px-4 py-2 text-left">Tempo</th>
-              <th className="border px-4 py-2 text-left">Predição</th>
-              <th className="border px-4 py-2 text-left">Resultado do teste</th>
+              <th className="border px-4 py-2 text-left">Predição (há falha?)</th>
+              <th className="border px-4 py-2 text-left">Resultado do teste (há falha?)</th>
             </tr>
           </thead>
           <tbody>
@@ -89,8 +94,8 @@ function HistoryPage() {
               <tr key={index} className="table-row">
                 <td className="border px-4 py-2">{item.KNR}</td>
                 <td className="border px-4 py-2">{renderTableRow(item.HALLE_TIMES)}</td>
-                <td className="border px-4 py-2">-</td>
-                <td className="border px-4 py-2">-</td>
+                <td className="border px-4 py-2">{item.OUTPUT_MODELO === null ? "-" : (item.OUTPUT_MODELO ? "S" : "N")}</td>
+                <td className="border px-4 py-2">{item.RESULTADO_TESTE === null ? "-" : (item.RESULTADO_TESTE ? "S" : "N")}</td>
               </tr>
             ))}
           </tbody>
