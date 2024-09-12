7de5f7be40e987c2864dfc37d0775e4eea3f1e08
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2024-09-12 17:56:23 -0300

    olha ai pedrao2

--- src/health/routers/health.py
+++ src/health/routers/health.py
@@ -5,22 +5,28 @@ router = APIRouter(tags=["health"])
 
 @router.get("/health", status_code=status.HTTP_200_OK)
 async def health_check():
-    backend_url = "http://localhost:3001/health_backend"
-    frontend_url = "http://localhost:3000/api/health"
+    # No seu código health_check
+    backend_url = "http://itcross-backend:3001/health_backend"
+    frontend_url = "http://itcross-frontend:3000/api/health"
+
 
     async with httpx.AsyncClient() as client:
         try:
             # Faz requisição ao backend
+            print('chamando backend')
             backend_response = await client.get(backend_url)
             backend_response.raise_for_status()
             backend_health = backend_response.json()
+            print(backend_health)
 
             # Faz requisição ao frontend
+            print('chamando frontend')
             frontend_response = await client.get(frontend_url)
             frontend_response.raise_for_status()
             frontend_health = frontend_response.json()
+            print(frontend_health)
 
-        except httpx.HTTPStatusError as e:
+        except Exception as e:
             raise HTTPException(
                 status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
                 detail=f"Error reaching one of the services: {str(e)}"
