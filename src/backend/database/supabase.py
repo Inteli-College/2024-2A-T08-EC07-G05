a4688e1a3ab7270b60ed9b5515ea92553909aebe
Marco Antonio Rizzi Meneguetti <123962801+Rizzi26@users.noreply.github.com>
2024-09-13 15:45:21 -0300

    Update supabase.py

--- src/backend/database/supabase.py
+++ src/backend/database/supabase.py
@@ -8,7 +8,7 @@ load_dotenv(find_dotenv())
 api_url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("SUPABASE_KEY")
 
-def create_supabase_client(m):
+def create_supabase_client():
     supabase: Client = create_client(api_url, key)
     return supabase
 
