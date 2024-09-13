dea1eab4ef9b5a88fb345424af14eb30ab49da3c
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2024-09-13 08:53:08 -0300

    tabela health

--- src/frontend/src/app/page.js
+++ src/frontend/src/app/page.js
@@ -1,7 +1,9 @@
+import Health from "/home/rizzi26/Documents/GitHub/faculdade/2024-2A-T08-EC07-G05/src/frontend/src/app/health/health.jsx";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>oi tudo bemm!!</p>
+      <Health />
     </main>
   );
 }
