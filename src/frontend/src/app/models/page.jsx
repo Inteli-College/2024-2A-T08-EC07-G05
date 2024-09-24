93c2853146812cfcfaab47fdca6299faffd51461
josevalencar <jose.silva@sou.inteli.edu.br>
2024-09-24 16:47:16 -0300

    FEAT initial data and models page

---
+++ src/frontend/src/app/models/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React, {useEffect, useState} from 'react';
+import "./style.css";
+
+function DataPage() {
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      Página de Dados
+    </div>
+  );
+}
+
+export default DataPage;
