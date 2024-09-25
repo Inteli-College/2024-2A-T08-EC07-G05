6805512208a6fd01037e5ca18c41929602302ccb
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-09-25 14:24:34 -0300

    feat: healthchek front showing errors info on table

--- src/frontend/src/app/health/page.jsx
+++ src/frontend/src/app/health/page.jsx
@@ -3,75 +3,74 @@ import React, { useEffect, useState } from 'react';
 
 function HealthPage() {
   const [health, setHealth] = useState([
-    { name: "Backend", health: "unhealth" },
-    { name: "Frontend", health: "unhealth" },
-    { name: "Data_Lake", health: "unhealth" },
-    { name: "Banco_De_Dados", health: "unhealth" },
+    { name: "Backend", health: "unhealthy" },
+    { name: "Frontend", health: "unhealthy" },
+    { name: "Data lake", health: "unhealthy" },
+    { name: "Banco de dados", health: "unhealthy" },
   ]);
 
   useEffect(() => {
-    // Função para realizar a requisição
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:5000/health'); // Substitua 'SUA_URL_AQUI' pela URL da sua API
+        const response = await fetch('http://localhost:5000/health');
         const data = await response.json();
 
-        // Mapeando a resposta da API para o estado de `health`
-
         const updatedHealth = [
-          { name: "Backend", health: data.backend_connection.status },
-          { name: "Frontend", health: data.frontend_connection.status },
-          { name: "Data_Lake", health: data.datalake_connection.status },
-          { name: "Banco_De_Dados", health: data.database_connection.status },
+          { name: "Backend", health: data.backend_connection.status, error: data.backend_connection.error },
+          { name: "Frontend", health: data.frontend_connection.status, error: data.frontend_connection.error },
+          { name: "Data lake", health: data.datalake_connection.status, error: data.datalake_connection.error },
+          { name: "Banco de dados", health: data.database_connection.status, error: data.datalake_connection.error },
         ];
 
-        console.log(updatedHealth)
-
-        setHealth(updatedHealth); // Atualiza o estado com os novos dados
+        setHealth(updatedHealth);
       } catch (error) {
         console.error('Erro ao buscar dados:', error);
       }
     };
 
-    // Chama fetchData a cada 30 segundos
     const intervalId = setInterval(() => {
       fetchData();
-    }, 1000); // 30 segundos em milissegundos
+    }, 30000);
 
-    // Chama a função pela primeira vez quando o componente é montado
     fetchData();
 
-    // Limpa o intervalo quando o componente é desmontado
     return () => clearInterval(intervalId);
   }, []);
 
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <h1 className="text-4xl text-center">Healthcheck:</h1>
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Service name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Status
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {health.map((service, index) => (
-            <tr key={index} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {service.name}
-              </th>
-              <td className={`px-6 py-4 ${service.health === 'healthy' ? 'text-green-600' : 'text-red-600'}`}>
-                {service.health}
-              </td>
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-full max-w-3xl p-10">
+        <h1 className="text-4xl text-center mb-6">Healthcheck:</h1>
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 bg-gray-100 rounded-lg shadow-lg overflow-hidden">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">Serviço</th>
+              <th scope="col" className="px-6 py-3">Status</th>
+              <th scope="col" className="px-6 py-3">Descrição de erro</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {health.map((service, index) => (
+              <tr key={index} className="bg-gray-100 border-b last:border-none">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 dark:text-white"
+                >
+                  {service.name}
+                </th>
+                <td
+                  className={`px-6 py-4 ${service.health === 'healthy' ? 'text-green-600' : 'text-red-600'}`}
+                >
+                  {service.health}
+                </td>
+                <td className="px-6 py-4">
+                  {service.error ? service.error : 'N/A'}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
