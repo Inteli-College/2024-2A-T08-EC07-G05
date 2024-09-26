4decbec88c578b0ce31117a58712f647d8846372
Murilo Prianti <murilo.silva@sou.inteli.edu.br>
2024-09-26 18:10:17 -0300

    comentários para fazermos amanha

--- src/backend/services/model.py
+++ src/backend/services/model.py
@@ -5,6 +5,9 @@ from datetime import datetime
 
 def get_model_by_id():
     data = get_by_id('Modelo', 'ID_MODELO,DATA_TREINO,PRECISAO')
+
+    # PEGAR NO BICKET DO SUPABASE O ARQUIVO PKL CUJO ID É DATA.ID_BUCKET
+    # RETORNAR ARQUIVO PKL 
     print(data)
     # for entry in data:
     #     id = entry['ID_MODELO']
@@ -14,6 +17,9 @@ def get_model_by_id():
 
 
 def create_model_by_id(precisao: float):
+    ## add o parametro do modelo (pkl) e data
+    # função que insere o pkl no bucket e retorna o id do bucket
+    # insere as metricas + id do bucket na tabela do supabase
     data = insert_table('Modelo', {"DATA_TREINO": datetime.now, "PRECISAO": precisao})
     parsed_data = parse_halle_times(data)
     print(data)
