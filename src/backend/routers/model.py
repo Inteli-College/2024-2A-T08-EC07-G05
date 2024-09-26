38543c221d5a297a2e4dd6df6b3115309ff93a14
Murilo Prianti <murilo.silva@sou.inteli.edu.br>
2024-09-26 18:10:17 -0300

    comentários para fazermos amanha

--- src/backend/routers/model.py
+++ src/backend/routers/model.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends
 from services.model import get_model_by_id, create_model_by_id
-from schemas.schemas import KNRInput
 from supabase import Client
 # from pydantic import BaseModel
 
@@ -10,7 +9,7 @@ router = APIRouter(tags=["model"])
 def get_supabase_client() -> Client:
     return create_supabase_client()
 
-@router.get("/getModel")
+@router.get("/getModel/")
 async def get_model(ID_MODELO: int):
     return get_model_by_id(ID_MODELO: int)
 
@@ -22,5 +21,7 @@ async def get_model():
 #     precisao: float
 
 @router.post("/createModel/")
+## RECEBER METRICAS E O ARQUIVO PKL
 async def get_model(precisao: float):
+ # função no services/model 
     return create_model_by_id(precisao)
