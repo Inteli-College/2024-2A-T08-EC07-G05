87c1693d4a0ea0a0fa8c9c4921ee1df0816eb9d7
23f09crz <pedroaccruz@gmail.com>
2024-10-07 14:37:52 -0300

    merging

--- src/backend/database/supabase.py
+++ src/backend/database/supabase.py
@@ -1,5 +1,6 @@
 from supabase import Client, create_client
 import os
+import tempfile
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -21,7 +22,107 @@ def query_table(table: str, columns: str):
         return response.data
     except Exception as e:
         print("An error occurred:", e)
+def insert_table(table: str, data: dict):
+    """
+    Table deve ser o nome da tabela, como 'Operacao'.
+    Data deve ser um dicionário com os dados a serem inseridos, como {'KNR': '123', 'HALLE': '123', 'TEMPO': '123'}
+    """
+    supabase = create_supabase_client()
+    try:
+        response = supabase.table(table).insert(data).execute()
+        return response.data
+    except Exception as e:
+        print("An error occurred:", e)
 
+def get_by_id(table: str, columns: str, id: int):
+    """
+    Table deve ser o nome da tabela, como 'Operacao'.
+    Columns deve ser uma string que lista as tabelas, como 'KNR, HALLE, TEMPO' (Por algum motivo satânico)
+    ID é o ID do modelo a ser buscado
+    """
+    supabase = create_supabase_client()
+    try:
+        response = supabase.table(table).select(columns).eq('ID_MODELO', id).execute()
+        return response.data
+    except Exception as e:
+        print("An error occurred:", e)
+
+def get_last_register(table: str, column: str):
+    """
+    Table deve ser o nome da tabela, como 'Operacao'.
+    Columns deve ser uma string que lista as tabelas, como 'KNR, HALLE, TEMPO' (Por algum motivo satânico)
+    ID é o ID do modelo a ser buscado
+    """
+    supabase = create_supabase_client()
+    try:
+        response = supabase.select([table]).order_by(table.c.column.desc()).limit(1)
+        return response.data
+    except Exception as e:
+        print("An error occurred:", e)
+
+def save_model_to_bucket(model_bytes: bytes, filename: str, bucketname: str):
+    supabase = create_supabase_client()
+    try:
+        response = supabase.storage.from_(bucketname).upload(filename, model_bytes)
+
+        if response.status_code == 200:
+            model_url = supabase.storage.from_(bucketname).get_public_url(filename)
+            return model_url
+        else:
+            print("Erro ao salvar o modelo no bucket:", response.json())
+            return None
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+    
+def get_model_from_bucket(filename: str, bucketname: str):
+
+    supabase = create_supabase_client()
+    try:
+        # Defina a parte base da URL (até o nome do bucket)
+        base_url = f"https://ujidmgiotjewdzkhutmy.supabase.co/storage/v1/object/public/{bucketname}/"
+        
+        # Remover a parte base da URL para obter o caminho do arquivo
+        file_path = file_url.replace(base_url, "").split("?")[0]
+
+        # Agora fazemos o download do arquivo
+        response = supabase.storage.from_(bucketname).download(file_path)
+        
+        if response.status_code == 200:
+            # Retornar o conteúdo do arquivo
+            return response.content
+        else:
+            print("Erro ao baixar o arquivo:", response.json())
+            return None
+    except Exception as e:
+        print("An error occurred while fetching the file:", e)
+        return None
+
+def delete_model_from_bucket(filename: str, bucketname: str):
+    supabase = create_supabase_client()
+    try:
+        # Tenta apagar o arquivo do bucket
+        response = supabase.storage.from_(bucketname).remove([filename])
+
+        if response:
+            print(f"Arquivo '{filename}' deletado com sucesso do bucket '{bucketname}'.")
+            return True
+        else:
+            print("Erro ao deletar o arquivo do bucket:", response.json())
+            return False
+    except Exception as e:
+        print("An error occurred while deleting the file:", e)
+        return False
+
+def delete_model_from_table(id):
+    supabase = create_supabase_client()
+    try:
+        response = supabase.table('Modelo').delete().eq('ID_MODELO', id).execute()
+        if response:
+            return response.data
+    except Exception as e:
+        print("An error occurred while deleting the model:", e)
+        return False
 def insert_dataframe_to_etl(df: pd.DataFrame):
     """
     Adiciona um dataframe pandas na tabela de ETL.
